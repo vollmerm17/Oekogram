91ad3ae1cc06a5f0b2d9479515e54da0a3b41ae1
markusAUT-FH <markus.gollmann@edu.fh-joanneum.at>
2020-01-08 10:31:43 +0100

    GridLayout Responisve
    Responsive Design

--- SWENGSFrontend/src/app/app.component.ts
+++ SWENGSFrontend/src/app/app.component.ts
@@ -1,11 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -13,14 +7,44 @@ export interface Tile {
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
-  tiles: Tile[] = [
-    {text: 'Headline', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Profile Icons', cols: 1, rows: 1, color: 'lightgreen'},
-    {text: 'Write Posts Area', cols: 2, rows: 2, color: 'lightpink'},
-    {text: 'Profile', cols: 1, rows: 4, color: 'lightgreen'},
-    {text: 'Recent Posts / Search / Profile', cols: 2, rows: 3, color: '#DDBDF1'},
-  ];
+export class AppComponent implements OnInit {
+
+  colspan;
+  colspanProfile;
+  colspanPosts;
+  rowheightPost;
+  rowhheightProfile;
+  panelOpenState = false;
+
+  ngOnInit() {
+    this.calculateColSpan();
+    this.calculateRowHeight();
+  }
+
+  calculateColSpan() {
+    if (window.innerWidth <= 400) {
+      this.colspan = 1;
+      this.colspanPosts = 1;
+      this.colspanProfile = 1;
+    } else {
+      this.colspan = 3;
+      this.colspanPosts = 2;
+      this.colspanProfile = 1;
+    }
+  }
+
+  onResize() {
+    this.calculateColSpan();
+    this.calculateRowHeight();
+  }
+
+  calculateRowHeight() {
+    // 30 * Anzahl der Posts
+    this.rowheightPost = 25 * 1;
+    this.rowhheightProfile = 4 + this.rowheightPost;
+
+  }
+
 }
 
 
