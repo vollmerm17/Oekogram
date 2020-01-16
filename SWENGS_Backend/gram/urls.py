4884c46cef3972794eb16e44a7afa782fb4255c5
vollmerm17 <45813151+vollmerm17@users.noreply.github.com>
2020-01-16 19:10:58 +0100

    Update urls.py
    File Upload

--- SWENGS_Backend/gram/urls.py
+++ SWENGS_Backend/gram/urls.py
@@ -6,6 +6,7 @@ from drf_yasg.views import get_schema_view
 from rest_framework_jwt.views import obtain_jwt_token
 
 from . import views
+from .views import FileUploadView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -46,7 +47,11 @@ urlpatterns = [
     path('blocked/<int:pk>/delete', views.blocked_delete),
     path('blocked/<slug:username>/add', views.block_add),
 
+
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    url(r'^media$', FileUploadView.as_view()),
+    path('media/<int:pk>', views.media_download),
+    path('media/<int:pk>/get', views.media_get),
 ]
