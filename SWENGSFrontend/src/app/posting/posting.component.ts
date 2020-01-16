ffe7ae10266ca7b2da79a8ded2c371b67ffb029d
markusAUT-FH <markus.gollmann@edu.fh-joanneum.at>
2020-01-16 11:33:33 +0100

    Posting Service

--- SWENGSFrontend/src/app/posting/posting.component.ts
+++ SWENGSFrontend/src/app/posting/posting.component.ts
@@ -1,18 +1,41 @@
 import {Component, OnInit} from '@angular/core';
+import {PostsService} from '../service/posts.service';
+
+export interface FrontEndPostInterface {
+  title: string;
+  content: string;
+  date: string;
+  username: string;
+}
 
 @Component({
   selector: 'app-posting',
   templateUrl: './posting.component.html',
   styleUrls: ['./posting.component.scss']
 })
+
+
+
 export class PostingComponent implements OnInit {
 
-  constructor() {
+  posts: any[];
+  frontEndPost: FrontEndPostInterface[];
+
+  constructor(public postService: PostsService) {
   }
 
-  panelOpenState = false;
+  // panelOpenState = false;
 
   ngOnInit() {
+    this.postService.getAllPosts().subscribe((response: any[]) => {this.posts = response; });
+
+    this.posts.forEach(function(value) {
+      this.frontEndPost.title = value.title;
+      this.frontEndPost.date = 'DD.MM.YYYY';
+      this.frontEndPost.content = value.content;
+      this.frontEndPost.username = this.postService.getUserName(value.user_id);
+    });
+
   }
 
 }
